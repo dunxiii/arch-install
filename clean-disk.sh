e553a81e4ddcea07a207040c9fbc34e32cd39f1f
Jack Söderblom <dunxiii@gmail.com>
2017-01-07 18:38:18 +0100

    more reliable

--- clean-disk.sh
+++ clean-disk.sh
@@ -6,6 +6,7 @@ swapoff --all
 #umount --verbose --lazy "${MOUNT_POINT}"
 umount --verbose --lazy /mnt
 vgchange -an /dev/mapper/vg
-pvremove /dev/mapper/lvm --force --force
+vgremove /dev/mapper/vg --force
+pvremove /dev/mapper/lvm --force
 cryptsetup luksClose /dev/mapper/lvm
 dd if=/dev/zero of=/dev/sda bs=512 count=1
